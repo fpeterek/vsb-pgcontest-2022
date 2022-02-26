94cf02402f9a4f9600a8abce6cdcab393cc2be57
Filip Peterek <filip.peterek@firma.seznam.cz>
2022-02-26 21:08:14 +0100

    Revert "Retry smarter loading"
    This reverts commit a6441018efedeca4f10b76b8a61ba8a2c5a3239b.

--- include/record_loader.hpp
+++ include/record_loader.hpp
@@ -18,23 +18,10 @@ namespace mc = moodycamel;
 
 class RecordLoader {
 
-    // 1 MB worth of 32 bit integers
-    static constexpr std::size_t bufferSize = 1024*1024 / sizeof(std::uint32_t);
-
     std::ifstream in;
     mc::BlockingConcurrentQueue<std::vector<Record>> & queue;
+    uint32_t buffer[50'000]{};
     std::vector<Record> records;
-    std::array<std::uint32_t, bufferSize> buffer;
-    std::size_t idx = bufferSize;
-
-    void loadToVector(std::vector<std::uint32_t> & dest, std::uint32_t toCopy);
-    void loadToBuffer();
-    std::uint32_t loadSize();
-
-    std::uint32_t lastRead() const;
-    std::size_t availableItems() const;
-    bool isEmpty() const;
-    bool isFinished() const;
 
 public:
 
